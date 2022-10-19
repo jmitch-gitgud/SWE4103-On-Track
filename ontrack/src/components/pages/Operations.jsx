0286a652becf6e250cd34e00600148a83564ee57
carlygmackenzie <carlygmackenzie@gmail.com>
2022-10-19 17:21:14 -0300

    added new pages

---
+++ ontrack/src/components/pages/Operations.jsx
@@ -0,0 +1,18 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import React from "react";
+
+
+function Operations(){
+    return(
+        <div>
+            <Header />
+            <div className="login-header padding-top-64">
+                <h1 className="padding-bottom-24">Welcome Operations Manager</h1>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default Operations;
---
+++ ontrack/src/components/pages/VP.jsx
@@ -0,0 +1,18 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import React from "react";
+
+
+function VP(){
+    return(
+        <div>
+            <Header />
+            <div className="login-header padding-top-64">
+                <h1 className="padding-bottom-24">Welcome Vice Principal</h1>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default VP;
