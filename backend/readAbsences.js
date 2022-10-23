68fe94b9194fb188a620231ff3709172295238b2
emmak <ewalsh3@unb.ca>
2022-10-23 18:08:49 -0300

    Added POST route

--- backend/readAbsences.js
+++ backend/readAbsences.js
@@ -1,35 +1,103 @@
-const { Pool, Client } = require('pg');
+var express = require("express");
+var bodyParser = require('body-parser')
+const app = express();
+var http = require("http");
+const {Client} = require("pg");
+var server = http.createServer(app);
 
-const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    database: "postgres",
-    password: "postgres",
-    port: 5432
-  });
+const listenPort = 3001;
+
+app.use(bodyParser.json({limit: '1mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '1mb', extended: true}))
+
+// Allow CORS support and remote requests to the service
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  next();
+});
+
+const PORT = 3001;
+
+app.route('/getAbsences')
+.get((req, res, next) => {
+  let staff_id = req.query.staff_id
+  const text = 'SELECT * FROM "SWE4103_Schema".work_abscense WHERE staff_id = ' + staff_id
 
 
- function readAbsences(){
-  const query1 = 'SELECT * FROM "SWE4103_Schema"."Work_Abscense" ORDER BY "Absence_ID" ASC';
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  database: "postgres",
+  password: "postgres",
+  port: 5432
+});
 
-  pool.connect(err => {
+  client.connect(err => {
     if (err) {
       console.error('connection error', err.stack)
     } else {
-      pool.query(query1, (err, res) => {
-        if(err){
-            return err;
-        }
-        else{
-            return res;
-        }
-      });
-    }
+      client.query(text, (err, pgres) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({status: "Invalid credentials"}));
+        } else {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({names : pgres.rows}));
+          }
+        });
+          }
+  })
+  client.close();
+})
+.post((req, res, next) => {
+  console.log("post")
+res.send('POST request called');
+})
+.all((req, res, next) => {
+  console.log("all")
+res.send('Other requests called');
+})
+  
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
 });
-}
 
-const val = readAbsences();
-console.log(val);
+// const { Pool, Client } = require('pg');
+
+// const pool = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     database: "postgres",
+//     password: "postgres",
+//     port: 5432
+//   });
 
 
-module.exports = readAbsences;
+//  function readAbsences(){
+//   const query1 = 'SELECT * FROM "SWE4103_Schema"."Work_Abscense" ORDER BY "Absence_ID" ASC';
+
+//   pool.connect(err => {
+//     if (err) {
+//       console.error('connection error', err.stack)
+//     } else {
+//       pool.query(query1, (err, res) => {
+//         if(err){
+//             return err;
+//         }
+//         else{
+//             return res;
+//         }
+//       });
+//     }
+// });
+// }
+
+// const val = readAbsences();
+// console.log(val);
+
+
+// module.exports = readAbsences;
